b89723b27bf81ecf5dd402973fc5e68c8a0267d1
williamgao-dev <35654586+williamgao-dev@users.noreply.github.com>
2018-03-03 23:17:52 +1100

    Update #2 - 02/03/18
    Welcome to my second commit!
    (this was a recommit due to incorrect information being published.)
    
    In this commit:
    - Tilemap was added. (refer to map.txt!)
    - Added functionality to game.load_data() to ensure variable gameFolder refers to the root folder that main.py is in
    - Fixed up some code, made extra comments for readibility
    
    Projected changes for next commit:
    - Further advancements to tilemap
    - Making movement smooth

--- sprites.py
+++ sprites.py
@@ -1,10 +1,13 @@
 import pygame as pg
 from settings import *
 
+# Player sprite class
 class Player(pg.sprite.Sprite):
     def __init__(self,game,x,y):
+        # Initalize an instance of Player in group all_sprites
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self,self.groups)
+        # Sets attributes of class Player, sets a reference to the game class
         self.game = game
         self.image = pg.Surface((TILESIZE,TILESIZE))
         self.image.fill(YELLOW)
@@ -12,12 +15,39 @@ class Player(pg.sprite.Sprite):
         self.x = x
         self.y = y
 
+    # Function to move the Player object in various directions
     def move(self,dx=0,dy=0):
-        self.x += dx
-        self.y += dy
+        if self.wallCollision(dx,dy) == False:
+            self.x += dx
+            self.y += dy
 
+    # Function to test if next movement of the Player object will collide with an instance of a wall.
+    # If the next movement collides, the function will return True.
+    def wallCollision(self,dx=0,dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        return False
+
+    # Function that updates the current position of the sprites EVERY frame.
     def update(self):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
 
+# Wall sprite class
 class Wall(pg.sprite.Sprite):
+    def __init__(self,game,x,y):
+        # Defines which groups the sprite should be in
+        self.groups = game.all_sprites, game.walls
+        # Initalizes into groups
+        pg.sprite.Sprite.__init__(self,self.groups)
+        self.game = game
+
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        # Makes the wall the size of a tile
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
